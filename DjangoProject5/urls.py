cd0c1f82deef4f9f74df9c57d51db2ffb79f5328
eldux123 <eeguerrerr@eafit.edu.co>
2025-09-02 17:41:48 -0500

    siguiente

--- DjangoProject5/urls.py
+++ DjangoProject5/urls.py
@@ -5,7 +5,11 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def discover(request):
+    return render(request, 'discover.html')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', home, name='home'),                 # Ruta principal
+    path('discover/', discover, name='discover') # Nueva ruta
 ]
