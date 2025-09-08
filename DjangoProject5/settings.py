ec0d5d2197666b0908eab9eda285a3e73fcd0a79
eldux123 <eeguerrerr@eafit.edu.co>
2025-09-08 13:24:12 -0500

    hastaahora9/8/2025

--- DjangoProject5/settings.py
+++ DjangoProject5/settings.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -72,3 +74,5 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
